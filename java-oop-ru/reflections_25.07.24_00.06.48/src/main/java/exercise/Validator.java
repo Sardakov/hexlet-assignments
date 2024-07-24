d0796c3711497f6b4cae980883d1e707303f862a
@hexlet/cli <support@hexlet.io>
2024-07-25 00:07:06 +0500

    submit java-oop-ru/reflections_25.07.24_00.06.48

---
+++ java-oop-ru/reflections_25.07.24_00.06.48/src/main/java/exercise/Validator.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.IntStream;
+
+// BEGIN
+public class Validator {
+    public static List<String> validate(Address object) {
+        List<String> result = new ArrayList<>();
+        Class<?> aClass = object.getClass();
+        Field[] fields = aClass.getDeclaredFields();
+        for (var field : fields) {
+            if (field.isAnnotationPresent(NotNull.class)) {
+                try {
+                    field.setAccessible(true);
+                    var value = field.get(object);
+                    if (value == null) {
+                        result.add(field.getName());
+                    }
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+        return result;
+    }
+    public static Map<String, List> advancedValidate(Address data) {
+
+        return null;
+    }
+}
+
+
