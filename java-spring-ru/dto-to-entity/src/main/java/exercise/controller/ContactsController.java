09095a346d72d5a4dbfe39acee6f83d728b083da
@hexlet/cli <support@hexlet.io>
2024-11-13 21:34:56 +0500

    submit java-spring-ru/dto-to-entity

---
+++ java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -0,0 +1,52 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import exercise.model.Contact;
+import exercise.repository.ContactRepository;
+import exercise.dto.ContactDTO;
+import exercise.dto.ContactCreateDTO;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // BEGIN
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO contactData) {
+        var contact = toEntity(contactData);
+        contactRepository.save(contact);
+        var contactDTO = toDTO(contact);
+        return contactDTO;
+
+
+    }
+
+    private Contact toEntity(ContactCreateDTO createDTO) {
+        var contact = new Contact();
+        contact.setFirstName(createDTO.getFirstName());
+        contact.setLastName(createDTO.getLastName());
+        contact.setPhone(createDTO.getPhone());
+        return contact;
+    }
+
+    private ContactDTO toDTO(Contact contact) {
+        var dto = new ContactDTO();
+        dto.setId(contact.getId());
+        dto.setFirstName(contact.getFirstName());
+        dto.setLastName(contact.getLastName());
+        dto.setPhone(contact.getPhone());
+        dto.setCreatedAt(contact.getCreatedAt());
+        dto.setUpdatedAt(contact.getUpdatedAt());
+        return dto;
+    }
+    // END
+}
